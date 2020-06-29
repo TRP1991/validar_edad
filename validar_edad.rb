69a08118710dfbb3b8b980fe47c70403f02e239b
Tamara Rubilar <ta.rubilar@gmail.com>
2020-06-29 15:04:24 -0400

    programming validate age

--- validar_edad.rb
+++ validar_edad.rb
@@ -1,13 +1,11 @@
-def prueba (x = 2)
-    edad = ARGV[0].to_i
-    puts x
-    if edad >= 18
-       puts "es mayor"
-    else
-       puts "es menor"
-    end
+def validar_edad (edad)
+   if edad >= 18
+      puts "es mayor"
+   else
+      puts "es menor"
+   end
 end
 
-prueba
-prueba(3)
-prueba 3
+validar_edad (rand())
+validar_edad (rand(3))
+validar_edad rand 3
